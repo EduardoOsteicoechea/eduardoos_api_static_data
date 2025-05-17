d1209c8e3f6dc7c87a8bebc8ad179b35679ffcfa
eduar <eduar@EDUARDO>
2025-05-17 18:19:38 -0400

    test deploy

--- obj/Debug/net8.0/eduardoos_api_static_data.AssemblyInfo.cs
+++ obj/Debug/net8.0/eduardoos_api_static_data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eduardoos_api_static_data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5b84110491c2d0a1130cd6cb93608a48a1924f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41ccebdeca25a77e1f44a122dc1c72de271a5ea6")]
 [assembly: System.Reflection.AssemblyProductAttribute("eduardoos_api_static_data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eduardoos_api_static_data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
