0d72fbb7d2768785cd8df37969dfb3e09c49cdb9
eduar <eduar@EDUARDO>
2025-05-17 17:46:06 -0400

    test deploy

--- Program.cs
+++ Program.cs
@@ -3,8 +3,6 @@ using eduardoos_api_static_data;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/static_data", () => "Hello World!");
-
 app.MapBetweenTheRoleAndTheTragedy();
 
 app.Run(); 
