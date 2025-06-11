89218244172c4c48a58cd809dca25631dd33e7d0
eduar <eduar@EDUARDO>
2025-06-11 08:58:26 -0400

    uploading files

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddCors(options =>
 {
 	options.AddPolicy("AllowLocalhostReact", policy =>
 	{
-		policy.WithOrigins("http://localhost:5173")
+		policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
 		.AllowAnyHeader()
 		.AllowAnyMethod();
 	});
--- obj/Debug/net8.0/eduardoos_api_static_data.AssemblyInfo.cs
+++ obj/Debug/net8.0/eduardoos_api_static_data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eduardoos_api_static_data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cade201639410d0f6f89ae1d5f8ca0b8790bc00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3b18d1f8a2c403ed092eb4e8ddffbea0bc927c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("eduardoos_api_static_data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eduardoos_api_static_data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
